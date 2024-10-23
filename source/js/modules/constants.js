e6ddc6eefedd5a2bc71f3337e2c8c5dae36755c2
VladissGernet <skeimorr@gmail.com>
2024-10-23 10:20:58 +0400

    fix: добавил отступы и активировал swiper

--- source/js/modules/constants.js
+++ source/js/modules/constants.js
@@ -6,11 +6,11 @@ const SwiperPrizeDrawOptions = {
   SLIDES_PER_VIEW: 2,
   SPACE_BETWEEN_DEFAULT: 30,
   SPACE_BETWEEN_MOBILE: 16,
-  SPACE_BETWEEN_MOBILE_MIN: 2,
+  SPACE_BETWEEN_MOBILE_MIN: 10,
 };
 const SwiperBreakpoints = {
   MOBILE_MIN: 300,
-  MOBILE: 374,
+  MOBILE: 375,
   TABLET: 768,
 };
 
--- source/js/modules/init-prize-draw-swiper.js
+++ source/js/modules/init-prize-draw-swiper.js
@@ -2,7 +2,30 @@ import {Swiper} from './../vendor/swiper.min.js';
 import {SwiperPrizeDrawOptions, SwiperBreakpoints} from './constants.js';
 
 const initPrizeDrawSwiper = () => {
-  return new Swiper('.prize-draw-swiper', {
+  const containersWithSwiper = document.querySelectorAll('.prize-draw-swiper');
+
+  if (!containersWithSwiper.length) {
+    return;
+  }
+
+  // Добавляем swiper к каждому контейнеру
+  containersWithSwiper.forEach((container) => {
+    const swiperList = container.querySelector('.prize-draw__sub-list');
+    const swiperListItems = swiperList.querySelectorAll('.prize-draw__sub-item');
+    // Если количество слайдов равно максимальному количеству слайдов, которое может быть
+    // отображено в контейнере, то все равно активируем swiper путем добавления копий слайдов.
+    if (SwiperPrizeDrawOptions.SLIDES_PER_VIEW === swiperListItems.length) {
+      swiperListItems.forEach((item) => {
+        swiperList.appendChild(item.cloneNode(true));
+      });
+    }
+    createSwiper(container);
+  });
+};
+
+// Функция для создания экземпляра Swiper
+const createSwiper = (container) => {
+  return new Swiper(container, {
     slidesPerView: SwiperPrizeDrawOptions.SLIDES_PER_VIEW,
     spaceBetween: SwiperPrizeDrawOptions.SPACE_BETWEEN_DEFAULT,
     loop: true,
